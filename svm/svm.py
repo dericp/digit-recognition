cf9f1902f62afec2ebcc265558b2728769d3c36f
Deric Pang <dericp@cs.washington.edu>
2017-03-09 12:55:19 -0800

    huh...?

--- svm/svm.py
+++ svm/svm.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import random
 
 step_size = 1e-12 # best step size we found
 C = 5000 # best c value we found
@@ -10,25 +11,28 @@ lambda_val = 1e-6
 sigma = 10
 m = 3000
 dev = False
+PEGASOS = True
 
 
 def main():
-    df_train = pd.read_csv("../data/mnist_train.csv")
+    df_train = pd.read_csv('../data/mnist_train.csv')
     df_train['bias'] = 1
 
     if not dev:
-        X = transform(df_train.drop("label", axis=1).values)
-        y = df_train["label"].values
+        X = df_train.drop('label', axis=1).values
+        y = df_train['label'].values
 
-        df_test = pd.read_csv("../data/mnist_test.csv")
+        df_test = pd.read_csv('../data/mnist_test.csv')
         df_test['bias'] = 1
-        X_test = transform(df_test.drop("label", axis=1).values)
-        y_test = df_test["label"].values
+        X_test = df_test.drop('label', axis=1).values
+        y_test = df_test['label'].values
 
         w_s = {}  # a map from classification {0, ..., 9} to weight vector
         for class_val in range(10):
-            #w = pegasos(X, y, class_val, EPOCHS)
-            w = sgd(X, y, class_val, 1e-12, 10000, EPOCHS)
+            if PEGASOS:
+                w = pegasos(X, y, class_val, EPOCHS)
+            else:
+                w = sgd(X, y, class_val, step_size, C, EPOCHS)
             w_s[class_val] = w
 
         test_error = calculate_test_error(w_s, X_test, y_test)
@@ -39,16 +43,18 @@ def main():
                 print('step size: ', step_size, ' c: ', C)
 
                 df_train_dev, df_dev = np.split(df_train.sample(frac=1), [int(.8 * len(df_train))])
-                X = transform(df_train_dev.drop("label", axis=1).values)
-                y = df_train_dev["label"].values
+                X = transform(df_train_dev.drop('label', axis=1).values)
+                y = df_train_dev['label'].values
                 X_dev = transform(df_dev.drop("label", axis=1).values)
-                y_dev = df_dev["label"].values
+                y_dev = df_dev['label'].values
 
                 # Training the SVM classifiers
                 w_s = {} # a map from classification {0, ..., 9} to weight vector
                 for class_val in range(10):
-                    #w = pegasos(X, y, class_val, EPOCHS)
-                    w = sgd(X, y, class_val, step_size, C, EPOCHS)
+                    if PEGASOS:
+                        w = pegasos(X, y, class_val, EPOCHS)
+                    else:
+                        w = sgd(X, y, class_val, step_size, C, EPOCHS)
                     w_s[class_val] = w
 
                 test_error = calculate_test_error(w_s, X_dev, y_dev)
@@ -58,26 +64,22 @@ def main():
 # transforms X into m-dimensional feature vectors using RFF and RBF kernel
 # Make sure this function works for both 1D and 2D NumPy arrays.
 def transform(X):
-    return X
-    '''
+    np.random.seed(0)
     b = np.random.rand(m) * 2 * np.pi
-
-    if X.ndim == 1:
-        w = np.random.multivariate_normal(np.zeros(X.size), sigma**2 * np.identity(X.size), m)
-    else:
-        w = np.random.multivariate_normal(np.zeros(X.shape[1]), sigma**2 * np.identity(X.shape[1]), m)
-
-    transformed = (2.0 / m)**0.5 * np.cos(np.dot(X, np.transpose(w)) + b)
+    w = np.random.multivariate_normal(np.zeros(X.shape[1]), sigma**2 * np.identity(X.shape[1]), m)
+    transformed = (2.0 / m)**0.5 * np.cos(np.dot(X, w.T) + b)
     # feature normalization
     transformed = (transformed - np.mean(transformed, 0)) / np.std(transformed, 0)
 
-    return transformed'''
+    return transformed
 
 
 def sgd(X, y, class_val, step_size, C, epochs):
     w = np.zeros(len(X[0]))
     for epoch in range(epochs):
-        for point in range(len(X)):
+        points = list(range(len(X)))
+        random.shuffle(points)
+        for point in points:
             # is this point in the class we are looking for?
             if y[point] == class_val:
                 y_i = 1
@@ -93,26 +95,25 @@ def sgd(X, y, class_val, step_size, C, epochs):
 
 def pegasos(X, y, class_val, epochs):
     w = np.zeros(X.shape[1])
-    for t in range(1, epochs * X.shape[0]):
-        w = pegasos_update(w, X, y, class_val, t)
+    t = 1
+    for epoch in range(epochs):
+        points = list(range(X.shape[0]))
+        random.shuffle(points)
+        for point in points:
+            if y[point] == class_val:
+                y_i = 1
+            else:
+                y_i = -1
+
+            learning_rate = 1 / (lambda_val * t)
+
+            w = (1 - learning_rate * lambda_val) * w + learning_rate * hinge_loss_gradient(w, X[point], y_i)
+            # optional projection step
+            w = min(1, ((1 / lambda_val**0.5) / np.linalg.norm(w))) * w
+
+            t += 1
 
     return w
-
-
-def pegasos_update(w, X, y, class_val, t):
-    i = int(np.random.uniform(0, X.shape[0]))
-    if y[i] == class_val:
-        y_i = 1
-    else:
-        y_i = -1
-
-    learning_rate = 1 / (lambda_val * t)
-
-    new_w = (1 - learning_rate * lambda_val) * w + learning_rate * hinge_loss_gradient(w, X[i], y_i)
-    # optional projection step
-    new_w = min(1, ((1 / lambda_val**0.5) / np.linalg.norm(new_w))) * new_w
-
-    return new_w
 
 
 # calculate the gradient of the hinge loss function
@@ -138,6 +139,7 @@ def calculate_test_error(w_s, X_test, y_test):
                 max_margin = margin
                 max_class = class_val
 
+        print(max_class, ' ', y_test[idx])
         if max_class != y_test[idx]:
             misclassified += 1
 
